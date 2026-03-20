966fe91206af7c4ff5add3521fd1fc7f68f993d8
zengbao yu <43713212+yuzengbaao@users.noreply.github.com>
2026-03-21 02:32:23 +0800

    feat: implement frontend/src/components/marketplace/index.ts for task #spider_github_proactive_SolFoundry_solfoundry_25_v5

---
+++ frontend/src/components/marketplace/index.ts
@@ -0,0 +1,6 @@
+export { default as AgentCard } from './AgentCard';
+export { default as AgentFilters } from './AgentFilters';
+export { default as AgentDetailModal } from './AgentDetailModal';
+export { default as AgentComparison } from './AgentComparison';
+export { default as HireAgentModal } from './HireAgentModal';
+export { default as RegisterAgentCTA } from './RegisterAgentCTA';
